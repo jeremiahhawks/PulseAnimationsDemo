7a54022ab2ea5e1287bb3aeab0934d26f9c2717b
mallardknundrum <mallardknundrum@gmail.com>
2025-12-16 10:36:36 -0500

    Initial Commit

---
+++ PulseAnimationsDemo/PulseAnimationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseAnimationsDemoApp.swift
+//  PulseAnimationsDemo
+//
+//  Created by Jeremiah Hawks on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseAnimationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PulseAnimationsDemoTests/PulseAnimationsDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PulseAnimationsDemoTests.swift
+//  PulseAnimationsDemoTests
+//
+//  Created by Jeremiah Hawks on 12/16/25.
+//
+
+import Testing
+@testable import PulseAnimationsDemo
+
+struct PulseAnimationsDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
